a9985a3ef8d3627ced5eec8b89474f633ac69cc5
sojeongw <sojeongw.official@gmail.com>
2025-01-19 16:47:38 +0900

    배열에 데이터 추가

--- src/collection/array/Array.java
+++ src/collection/array/Array.java
@@ -32,5 +32,29 @@ public class Array {
                 break;
             }
         }
+
+        // 데이터 추가: O(n)
+        // 처음이나 중간에 추가하려면 기존 값을 오른쪽으로 한 칸씩 이동해줘야 한다.
+
+        // 첫 칸에 추가할 경우: 배열의 위치를 찾는 데 O(1) + 데이터 이동에 O(n) = O(n)
+        int newValue = 3;
+        // 뒤에서부터 하나씩 옮긴다.
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = newValue;
+        System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
+
+        // 중간에 추가할 경우: 배열의 위치를 찾는 데 O(1) + 데이터 이동에 O(n/2) = O(n)
+        int index = 2;
+        for (int i = arr.length - 1; i > index; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[index] = 4;
+        System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
+
+        // 마지막에 추가할 경우: 기존 배열을 이동하지 않으므로 위치 찾는 데만 O(1)
+        arr[arr.length-1] = 9;
+        System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
     }
 }
